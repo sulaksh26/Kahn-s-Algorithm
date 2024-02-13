f8e3afd3f9ca387ac64f6e1630757be8b89ef4cb
Sulaksh Dahiya <116136618+sulaksh26@users.noreply.github.com>
2024-02-13 17:48:28 +0530

    Add files via upload

---
+++ L22.cpp
@@ -0,0 +1,54 @@
+/*Kahn's Algorithm
+
+class Solution
+{
+	public:
+	
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    int indegree[V]={0};
+	    
+	    for(int i=0;i<V;i++){
+	        for(auto it: adj[i]){
+	            indegree[it]++;
+	        }
+	    }
+	    
+	    
+	    
+	    queue<int>q;
+	    for(int i=0;i<V;i++){
+	        if(indegree[i]==0){
+	            q.push(i);
+	        }
+	    }
+	    
+	    vector<int>topo;
+	    
+	    while(!q.empty()){
+	        int node=q.front();
+	        
+	        
+	        q.pop();
+	        
+	        topo.push_back(node);
+	        // Node is in your topo sort
+	        //So please remove it from the indegree
+	        
+	        for(auto it: adj[node]){
+	            indegree[it]--;
+	            
+	            if(indegree[it]==0){
+	                q.push(it);
+	            }
+	        }
+	    }
+	    return topo;
+	    
+	}
+};
+
+
+
+
+*/
